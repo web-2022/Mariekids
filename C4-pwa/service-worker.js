545c2e1eab285b38c8275bb5d94f96efcff4ac88
web2022 <98061099+web-2022@users.noreply.github.com>
2025-12-13 06:15:24 -0500

    Add files via upload
    Complit load

---
+++ C4-pwa/service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "c4-pwa-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./service-worker.js",
+  "./correct.mp3",
+  "./wrong.mp3",
+  "./levelup.mp3",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
